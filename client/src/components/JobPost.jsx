417459a126cdfd58222bff65dd0e4aa22013e67b
Bijeesh M <bijeeshh.m@gmail.com>
2025-01-15 10:38:27 +0530

    Implemented user profile

--- client/src/components/JobPost.jsx
+++ client/src/components/JobPost.jsx
@@ -1,25 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 
 const JobPost = () => {
+    const [jobData, setJobData] = useState({
+        title: "",
+        company: "",
+        location: "",
+        jobType: "",
+        salary: "",
+        description: "",
+    });
+
     return (
         <div>
             <form>
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">
-                        Email address
+                        Title
                     </label>
                     <input
-                        type="email"
+                        onChange={(e) => handleChange(e)}
+                        name="title"
+                        type="text"
                         class="form-control"
                         id="exampleFormControlInput1"
-                        placeholder="name@example.com"
                     />
                 </div>
                 <div class="mb-3">
-                    <label for="exampleFormControlTextarea1" class="form-label">
-                        Example textarea
+                    <label for="exampleFormControlInput1" class="form-label">
+                        company
                     </label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <input
+                        onChange={(e) => handleChange(e)}
+                        name="company"
+                        type="text"
+                        class="form-control"
+                        id="exampleFormControlInput1"
+                    />
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">
+                        Location
+                    </label>
+                    <input
+                        onChange={(e) => handleChange(e)}
+                        name="location"
+                        type="text"
+                        class="form-control"
+                        id="exampleFormControlInput1"
+                    />
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">
+                        company
+                    </label>
+                    <input
+                        onChange={(e) => handleChange(e)}
+                        name="company"
+                        type="text"
+                        class="form-control"
+                        id="exampleFormControlInput1"
+                    />
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">
+                        jobType
+                    </label>
+                    <input
+                        onChange={(e) => handleChange(e)}
+                        name="jobType"
+                        type="text"
+                        class="form-control"
+                        id="exampleFormControlInput1"
+                    />
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">
+                        salary
+                    </label>
+                    <input
+                        onChange={(e) => handleChange(e)}
+                        name="salary"
+                        type="number"
+                        class="form-control"
+                        id="exampleFormControlInput1"
+                    />
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">
+                        description
+                    </label>
+                    <input
+                        onChange={(e) => handleChange(e)}
+                        name="description"
+                        type="text"
+                        class="form-control"
+                        id="exampleFormControlInput1"
+                    />
                 </div>
             </form>
         </div>
